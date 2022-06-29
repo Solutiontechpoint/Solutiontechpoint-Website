301c273f25389d84d57f026173c3b7be8e1065c1
Thanu Ganesh <thanunr5@gmail.com>
2022-06-29 15:45:51 +0530

    contact form

--- forms/contact.php
+++ forms/contact.php
@@ -27,14 +27,12 @@
 
   // echo $contact->send();
   //$from = $_POST['email'];
-  $from = 'Your Name <sitename@hostname.com>';
+  $from = $_POST['name'] .'<'. $_POST['email'] .'>';
   $headers = "" .
            "Reply-To:" . $from . "\r\n" .
            "From:" . $from . "\r\n" .
            "X-Mailer: PHP/" . phpversion();
   $headers .= 'MIME-Version: 1.0' . "\r\n";
-  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";  
-  //$headers = 'From: '.$_POST['email'];
-  $subject = $_POST['name'] .' ('.$_POST['subject'] .')';
-  echo mail($receiving_email_address, $subject, $_POST['message'], $headers);
+  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+  echo mail($receiving_email_address, $$_POST['subject'], $_POST['message'], $headers);
 ?>
