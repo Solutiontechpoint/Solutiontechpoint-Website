b06d51c2ad5c0c14f91e1cee6cf711b8ac6a2a60
Thanu Ganesh <thanunr5@gmail.com>
2022-06-29 15:48:55 +0530

    contact form

--- forms/contact.php
+++ forms/contact.php
@@ -1,32 +1,7 @@
 <?php
 
-  $receiving_email_address = 'thanunr5@gmail.com';
+  $receiving_email_address = 'contact@solutiontechpoint.com';
 
-
-  // $contact = new PHP_Email_Form;
-  // $contact->ajax = true;
-  
-  // $contact->to = $receiving_email_address;
-  // $contact->from_name = $_POST['name'];
-  // $contact->from_email = $_POST['email'];
-  // $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  // $contact->add_message( $_POST['name'], 'From');
-  // $contact->add_message( $_POST['email'], 'Email');
-  // $contact->add_message( $_POST['message'], 'Message', 10);
-
-  // echo $contact->send();
-  //$from = $_POST['email'];
   $from = $_POST['name'] .'<'. $_POST['email'] .'>';
   $headers = "" .
            "Reply-To:" . $from . "\r\n" .
